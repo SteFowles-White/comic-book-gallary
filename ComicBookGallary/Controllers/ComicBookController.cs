f0838ce42ddba4ea1433519d22598791f2d5f5ed
Stephen Fowles-White <fowleswhite@gmail.com>
2019-02-19 21:51:38 +0000

    Added Comic Book Detail View

--- ComicBookGallary/Controllers/ComicBookController.cs
+++ ComicBookGallary/Controllers/ComicBookController.cs
@@ -11,13 +11,7 @@ namespace ComicBookGallary.Controllers
         public ActionResult Detail()
         {
 
-            if(DateTime.Today.DayOfWeek == DayOfWeek.Tuesday)
-            {
-                return Redirect("/");
-
-            }
-            return Content("Hello From the Comic Book Controller");
-
+            return View();
         }
 
     }
