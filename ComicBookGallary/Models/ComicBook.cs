2afc4b406bac22c194789474e74b28767d376694
Stephen Fowles-White <fowleswhite@gmail.com>
2019-02-22 22:27:49 +0000

    Added Comic Book Artist and models

---
+++ ComicBookGallary/Models/ComicBook.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ComicBookGallary.Models
+{
+    public class ComicBook
+    {
+        public int Id { get; set;}
+        public string SeriesTitle { get; set; }
+        public int IssueNumber { get; set; }
+        public string Description { get; set; }
+        public Artists[] Artists { get; set; }
+        public bool Favorite { get; set; }
+
+    }
+}
