0ec23e7593786429e63c94edf77b02d00310e902
Stephen Fowles-White <fowleswhite@gmail.com>
2019-02-22 22:45:09 +0000

    Updated Commic Book Model

--- ComicBookGallary/Models/ComicBook.cs
+++ ComicBookGallary/Models/ComicBook.cs
@@ -7,12 +7,29 @@ namespace ComicBookGallary.Models
 {
     public class ComicBook
     {
-        public int Id { get; set;}
+        public int Id { get; set; }
         public string SeriesTitle { get; set; }
         public int IssueNumber { get; set; }
         public string Description { get; set; }
         public Artists[] Artists { get; set; }
         public bool Favorite { get; set; }
+
+        public string DisplayText
+        {
+            get
+            {
+                return SeriesTitle + " #" + IssueNumber;
+            }
+        }
+
+        public string CoverImageFileName
+        {
+            get
+            {
+                return SeriesTitle.Replace(" ", "-")
+                    .ToLower() + "-" + IssueNumber + ".jpg";
+            }
+        }
 
     }
 }
