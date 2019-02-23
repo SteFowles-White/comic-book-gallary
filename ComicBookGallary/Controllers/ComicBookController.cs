585802d86e7c186bf19dd689f7909de32aa88084
Stephen Fowles-White <fowleswhite@gmail.com>
2019-02-23 20:41:47 +0000

    Changes the Comic data to be in the data File and removed it from the Controller

--- ComicBookGallary/Controllers/ComicBookController.cs
+++ ComicBookGallary/Controllers/ComicBookController.cs
@@ -12,24 +12,8 @@ namespace ComicBookGallary.Controllers
     {
         public ActionResult Detail()
         {
-            var comicBook = new ComicBook()
-            {
-                SeriesTitle = "The Amazing Spider-Man",
-                IssueNumber = 700,
-                DescriptionHtml = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
-                Artists = new Artists[]
-                     {
-                         new Artists() {Name = "Dan Slott", Role = "Script" },
-                         new Artists() {Name = "Pencils", Role = "Humberto Ramos" },
-                         new Artists() {Name = "Inks", Role = "Victor Olazaba" },
-                         new Artists() {Name = "Colors", Role = "Edgar Delgado" },
-                         new Artists() {Name = "Letters", Role = "Chris Eliopoulos" },
-                     },
 
-            };
-
-
-            return View(comicBook);
+            return View();
 
         }
 
